7ec401d26fb990b2fc5550a42c78362ab4b53213
jenisys <jenisys@users.noreply.github.com>
2019-12-23 16:39:23 +0100

    Remove _compat_shutil.py

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -32,16 +32,21 @@ sys.path.insert(0, HERE)
 # import sys
 # PREPARED: from invoke import task, Collection
 from invoke import Collection
-
-# -- TASK-LIBRARY:
-import invoke_cleanup as cleanup
-from . import test
-# DISABLED: from . import release
+try:
+    from shutil import which    # -- SINCE: Python 3.3
+except ImportError:
+    from backports.shutil_which import which
 
 
 # -----------------------------------------------------------------------------
 # TASKS:
 # -----------------------------------------------------------------------------
+# -- TASK-LIBRARY:
+import invoke_cleanup as cleanup
+from . import test
+# DISABLED: from . import release
+
+# -- TASKS:
 # None here.
 
 
@@ -60,7 +65,6 @@ cleanup.cleanup_tasks.add_task(cleanup.clean_python)
 namespace.configure(cleanup.namespace.configuration())
 if sys.platform.startswith("win"):
     # -- OVERRIDE SETTINGS: For platform=win32, ... (Windows)
-    from ._compat_shutil import which
     run_settings = dict(echo=True, pty=False, shell=which("cmd"))
     namespace.configure({"run": run_settings})
 else:
