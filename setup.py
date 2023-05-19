3ac82f9c6e9b215c1fdcbc271030ec553db5312e
jenisys <jenisys@users.noreply.github.com>
2023-05-19 20:36:34 +0200

    BUMP-VERSION: 0.3.7 (was: 0.3.6)

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ def find_packages_by_root_package(where):
 # SEE: https://setuptools.pypa.io/en/latest/userguide/declarative_config.html
 setup(
     name="invoke-cleanup",
-    version="0.3.6",
+    version="0.3.7",
     description="Performs cleanup tasks for the ``invoke`` build system",
     long_description=description,
     author="Jens Engel",
